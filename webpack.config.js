e9c3e72876c4233d451c3ccf831fa8eac8abd5c6
aniketh <sunnyrockzzz1811@gmail.com>
2024-09-27 09:07:40 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const htmlWebPackPlugin = require('html-webpack-plugin')
+const { watchFile } = require('fs')
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+
+    devtool: "eval-source-map",
+
+    devServer:{
+        watchFiles: ["./src/template.html"]
+    },
+
+    plugins: [
+        new htmlWebPackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+        ],
+      },
+}
